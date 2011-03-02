879e51f60e712ae702b092256f83f37066254246
David Norman <davidn@xmos.com>
2011-03-02 12:57:40 +0000

    Adding remaining copyright messages

--- module_zeroconf/mdns/netbios_protocol.h
+++ module_zeroconf/mdns/netbios_protocol.h
@@ -1,3 +1,8 @@
+// Copyright (c) 2011, XMOS Ltd, All rights reserved
+// This software is freely distributable under a derivative of the
+// University of Illinois/NCSA Open Source License posted in
+// LICENSE.txt and at <http://github.xcore.com/>
+
 /*
  * Redistribution and use in source and binary forms, with or without modification, 
  * are permitted provided that the following conditions are met:
