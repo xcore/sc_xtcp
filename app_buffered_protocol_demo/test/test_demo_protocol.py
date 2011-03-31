a79a1f5dc6767970e5ee7ab6da9467d94b9207b7
Dave Lacey <david.lacey@xmos.com>
2011-03-31 15:15:20 +0100

    Added buffered demo test script

---
+++ app_buffered_protocol_demo/test/test_demo_protocol.py
@@ -0,0 +1,31 @@
+import socket,sys
+
+#
+#sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+print "Connecting.."
+sock.connect((sys.argv[1], 15533))
+print "Connected"
+
+
+for i in range(0,10):
+
+    msg = "\0hello world "
+    for j in range(i):
+        msg = msg + str(j)
+
+    msg = msg + '\0'
+    msg = chr(len(msg)) + msg
+
+    msg = msg + msg
+
+    print "Sending message: " + msg[1:-1]
+    sock.send(msg)
+    reply = sock.recv(14)
+#    reply = sock.recv(14)
+    print "Reply: " + str([ord(x) for x in list(reply)])
+
+print "Closing..."
+sock.close()
+print "Closed"
